13882e9ce2cb9fbd2a0d97210e44300ef8a93bbb
HFwas <1558934404@qq.com>
2020-08-31 22:02:40 +0800

    "2020-8-31:阻塞队列,线程池"

--- JUC/src/com/atguigu/demo/BlockingQueueDemo.java
+++ JUC/src/com/atguigu/demo/BlockingQueueDemo.java
@@ -3,7 +3,7 @@ package com.atguigu.demo;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
-/*
+/*线程通信之消费者生产者阻塞队列版
 * ArrayBlockingQueue:是一个基于数组结构的有界阻塞队列，此队列按FIFO原则对元素进行排序
 * LinkedBlockingQueue:是一个基于链表结构的阻塞队列，此队列按FIFO排序元素，吞吐量高于ArrayBlockingQueue
 * SynchronousQueue：一个不存储元素的阻塞队列，每个插入操作必须等到另一个线程调用移出操作，否则插入操作一直处于
