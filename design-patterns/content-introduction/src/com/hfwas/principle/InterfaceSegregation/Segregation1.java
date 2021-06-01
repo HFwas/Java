9cb966944c64b3161debb16d808208aba211dbfe
Hfwas <1558934404@qq.com>
2021-06-01 23:05:19 +0800

    "学习杂记"

---
+++ design-patterns/content-introduction/src/com/hfwas/principle/InterfaceSegregation/Segregation1.java
@@ -0,0 +1,13 @@
+package com.hfwas.principle.InterfaceSegregation;
+
+/**
+ * @Author: HFwas
+ * @Date: 2021/1/15
+ * @Description: com.hfwas.principle.InterfaceSegregation
+ * @version: 1.0
+ */
+public class Segregation1 {
+    public static void main(String[] args) {
+        
+    }
+}
