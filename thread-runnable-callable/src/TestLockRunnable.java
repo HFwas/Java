7b30cbfdcb8eac228d2672eef77776fc90ca833a
HFwas <1558934404@qq.com>
2024-05-26 15:07:51 +0800

    juc thread

---
+++ thread-runnable-callable/src/TestLockRunnable.java
@@ -0,0 +1,29 @@
+public class TestLockRunnable {
+	public static void main(String[] args) {
+		MyRunnable1 myRunnable1 = new MyRunnable1();
+		Thread thread1 = new Thread(myRunnable1);
+		Thread thread2 = new Thread(myRunnable1);
+		Thread thread3 = new Thread(myRunnable1);
+		thread1.start();
+		thread2.start();
+		thread3.start();
+	}
+}
+
+class MyRunnable1 implements Runnable{
+	private int total = 1000;
+	@Override
+	public void run() {
+		while (true) {
+			synchronized (this) {//选择this当锁可以。因为只有一个MyRunnable1的对象
+				if (total >0) {
+					System.out.println(Thread.currentThread().getName() + "卖出一张票");
+					total--;
+					System.out.println("剩余：" + total);
+				}else {
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ thread-runnable-callable/src/TestRunnable.java
@@ -0,0 +1,23 @@
+//创建线程的第二种方式
+//实现Runnable接口
+public class TestRunnable {
+	public static void main(String[] args) {
+		MyRunnable myRunnable = new MyRunnable();
+		//Runnable没有start方法，需要借助Thread类的start方法：设计到设计模式中的代理模式
+		Thread thread = new Thread(myRunnable);
+		thread.start();
+		for (int i = 0; i < 20; i++) {
+			System.out.println("main方法："+i);
+		}
+	}
+}	
+
+class MyRunnable implements Runnable{
+	@Override
+	public void run() {
+		for (int i = 0; i < 30; i++) {
+			System.out.println("自定义线程："+ i );
+		}
+	}
+	
+}
